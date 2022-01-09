53cd2f32dfecb7273417c671261b3c37557c38d9
Максим Близнюк <bmu0@yandex.ru>
2022-01-10 01:38:23 +0300

    test

--- homework.py
+++ homework.py
@@ -118,8 +118,8 @@ def read_package(workout_type: str, data: list) -> Training:
 
 def main(training: Training) -> None:
     """Главная функция."""
-    info = Training.show_training_info(training)
-    print(InfoMessage.get_message(info))
+    info = training.show_training_info()
+    print(info.get_message())
 
 
 if __name__ == '__main__':
